eff4e3678917b4176e5542e1069a3251f608091f
grizeus <zaytsewww@gmail.com>
2024-12-06 16:33:45 +0200

    .

--- 05/pt-1.js
+++ 05/pt-1.js
@@ -1,19 +1,24 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
-const fileContent = (
-  await fs.readFile(path.join(process.cwd(), "test.txt"))
-).toString("utf8");
-
-const rawData = fileContent.split("\r\n");
-const order = rawData
-  .slice(0, rawData.indexOf(""))
-  .map((line) => line.split("|"))
-  .map(([a, b]) => [parseInt(a), parseInt(b)]);
-const updates = rawData
-  .slice(rawData.indexOf("") + 1)
-  .map((line) => line.split(",")).map(a => a.map(b => parseInt(b)));
-
-console.log(rawData);
-console.log(order);
-console.log(updates);
+const [order, updates] = (
+  await fs.readFile(path.join(process.cwd(), "test.txt"), "utf8")
+)
+  .split("\r\n\r\n")
+  .map((m) => m.split("\r\n"));
+console.log(
+  updates
+    .map((m) => {
+      let isValid = true;
+      const page_list = m.split(",");
+      console.log(page_list);
+      for (let i = 0; i < page_list.length - 1; i++) {
+        for (let j = i + 1; j < page_list.length; j++) {
+          const pair = page_list[i] + "|" + page_list[j];
+          isValid = isValid && order.indexOf(pair) !== -1;
+        }
+      }
+      return isValid ? +page_list[Math.floor(page_list.length / 2)] : 0;
+    })
+    .reduce((acc, curr) => acc + curr, 0)
+);
