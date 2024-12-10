de047ce0d8bdabf93237d0dd07fbef06e9bb3ea8
grizeus <zaytsewww@gmail.com>
2024-12-10 06:17:38 +0200

    .

--- 06/pt-1.js
+++ 06/pt-1.js
@@ -11,14 +11,19 @@ const cols = map[0].length;
 const directions = {
   up: 0,
   right: 1,
-  down: 3,
-  left: 2,
+  down: 2,
+  left: 3,
 };
 
+const isValidPosition = (row, col) => {
+    console.log(row, col, rows, cols);
+  return row >= 0 && row < rows && col >= 0 && col < cols;
+};
 class Guard {
   direction = directions.up;
   position = { x: 0, y: 0 };
   path = new Set();
+  map = map;
 
   constructor(x, y) {
     this.position = { x: x, y: y };
@@ -26,34 +31,58 @@ class Guard {
   turnRight() {
     if (this.direction === directions.left) {
       this.direction = directions.up;
+    } else {
+      this.direction++;
     }
-    this.direction++;
   }
 
-  setPosition(x, y) {
-    this.position = { x: x, y: y };
+  setPosition(row, col) {
+    this.position = { x: col, y: row };
+    if (isValidPosition(this.position.x, this.position.y)) {
+      this.addPath(this.position);
+    }
   }
 
   move() {
     switch (this.direction) {
       case directions.up: {
+        const { y, x } = this.position;
+        if (isValidPosition(x, y + 1) && this.map[y - 1][x] === "#") {
+          this.turnRight();
+          break;
+        }
         this.position.y--;
-        this.addPath(this.position.x, this.position.y);
+        this.addPath(this.position);
         break;
       }
       case directions.left: {
+        const { x, y } = this.position;
+        if (isValidPosition(x, y + 1) && this.map[y][x - 1] === "#") {
+          this.turnRight();
+          break;
+        }
         this.position.x--;
-        this.addPath(this.position.x, this.position.y);
+        this.addPath(this.position);
         break;
       }
       case directions.right: {
+        const { x, y } = this.position;
+        if (isValidPosition(x, y + 1) && this.map[y][x + 1] === "#") {
+          this.turnRight();
+          break;
+        }
         this.position.x++;
-        this.addPath(this.position.x, this.position.y);
+        this.addPath(this.position);
         break;
       }
       case directions.down: {
+        const { x, y } = this.position;
+        if (isValidPosition(x, y + 1) && this.map[y + 1][x] === "#") {
+          this.turnRight();
+          break;
+        }
         this.position.y++;
-        this.addPath(this.position.x, this.position.y);
+        this.addPath(this.position);
         break;
       }
       default:
@@ -61,28 +90,29 @@ class Guard {
     }
   }
 
-  addPath(x, y) {
-    if (!this.path.has([x, y])) {
-      this.path.add([x, y]);
+  addPath(pos) {
+    const pair = pos.x + "|" + pos.y;
+    if (!this.path.has(pair)) {
+      this.path.add(pair);
     }
   }
 }
 
 const guard = new Guard();
 
-for (let row = 0; row < rows; ++row) {
-  for (let col = 0; col < cols; ++col) {
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
     if (map[row][col] === "^") {
       guard.setPosition(row, col);
     }
   }
 }
+// console.log(guard);
 let position = guard.position;
-while (position.y >= 0 && position.y <= 10) {
+console.log(position);
+while (position.y >= 0 && position.y < rows && position.x >= 0 && position.x < cols) {
   guard.move();
-  console.log(position);
   position = guard.position;
 }
 
-console.log(map);
 console.log(guard);
