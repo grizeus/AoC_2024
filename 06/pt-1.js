a0b7f53dd63774baf228897043ff71c9db43a282
grizeus <zaytsewww@gmail.com>
2024-12-10 06:37:50 +0200

    day 6, pt 1

--- 06/pt-1.js
+++ 06/pt-1.js
@@ -1,12 +1,12 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
-const map = (await fs.readFile(path.join(process.cwd(), "test.txt"), "utf8"))
+const grid = (await fs.readFile(path.join(process.cwd(), "input.txt"), "utf8"))
   .split("\r\n")
   .map((line) => line.split(""));
 
-const rows = map.length;
-const cols = map[0].length;
+const rows = grid.length;
+const cols = grid[0].length;
 
 const directions = {
   up: 0,
@@ -16,17 +16,17 @@ const directions = {
 };
 
 const isValidPosition = (row, col) => {
-    console.log(row, col, rows, cols);
   return row >= 0 && row < rows && col >= 0 && col < cols;
 };
 class Guard {
   direction = directions.up;
   position = { x: 0, y: 0 };
   path = new Set();
-  map = map;
+  grid = [];
 
-  constructor(x, y) {
+  constructor(x, y, grid) {
     this.position = { x: x, y: y };
+    this.grid = grid;
   }
   turnRight() {
     if (this.direction === directions.left) {
@@ -38,16 +38,14 @@ class Guard {
 
   setPosition(row, col) {
     this.position = { x: col, y: row };
-    if (isValidPosition(this.position.x, this.position.y)) {
-      this.addPath(this.position);
-    }
+    this.addPath(this.position);
   }
 
   move() {
     switch (this.direction) {
       case directions.up: {
         const { y, x } = this.position;
-        if (isValidPosition(x, y + 1) && this.map[y - 1][x] === "#") {
+        if (isValidPosition(x, y + 1) && this.grid[y - 1][x] === "#") {
           this.turnRight();
           break;
         }
@@ -57,7 +55,7 @@ class Guard {
       }
       case directions.left: {
         const { x, y } = this.position;
-        if (isValidPosition(x, y + 1) && this.map[y][x - 1] === "#") {
+        if (isValidPosition(x, y + 1) && this.grid[y][x - 1] === "#") {
           this.turnRight();
           break;
         }
@@ -67,7 +65,7 @@ class Guard {
       }
       case directions.right: {
         const { x, y } = this.position;
-        if (isValidPosition(x, y + 1) && this.map[y][x + 1] === "#") {
+        if (isValidPosition(x, y + 1) && this.grid[y][x + 1] === "#") {
           this.turnRight();
           break;
         }
@@ -77,7 +75,7 @@ class Guard {
       }
       case directions.down: {
         const { x, y } = this.position;
-        if (isValidPosition(x, y + 1) && this.map[y + 1][x] === "#") {
+        if (isValidPosition(x, y + 1) && this.grid[y + 1][x] === "#") {
           this.turnRight();
           break;
         }
@@ -92,27 +90,30 @@ class Guard {
 
   addPath(pos) {
     const pair = pos.x + "|" + pos.y;
-    if (!this.path.has(pair)) {
+    if (isValidPosition(pos.x, pos.y) && !this.path.has(pair)) {
       this.path.add(pair);
     }
   }
 }
 
-const guard = new Guard();
+const guard = new Guard(0, 0, grid);
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
-    if (map[row][col] === "^") {
+    if (grid[row][col] === "^") {
       guard.setPosition(row, col);
     }
   }
 }
-// console.log(guard);
 let position = guard.position;
-console.log(position);
-while (position.y >= 0 && position.y < rows && position.x >= 0 && position.x < cols) {
+while (
+  position.y >= 0 &&
+  position.y < rows &&
+  position.x >= 0 &&
+  position.x < cols
+) {
   guard.move();
   position = guard.position;
 }
 
-console.log(guard);
+console.log(guard.path.size);
