51360277f215d5d1ed5f350267dc03456758a0bf
grizeus <zaytsewww@gmail.com>
2024-12-12 15:55:53 +0200

    .

--- 09/pt-1.js
+++ 09/pt-1.js
@@ -1,14 +1,12 @@
-import {
-  pathBuilder,
-  parseInput
-} from "../utils/utils.js";
+import { pathBuilder, parseInput } from "../utils/utils.js";
 
 const path = pathBuilder("test.txt");
-
+let fileInput;
 try {
-  const data = await parseInput(path);
-  console.log(data);
+  fileInput = await parseInput(path);
 } catch (error) {
   console.error(error.message);
 }
-
+const freeSpace = [...fileInput].map(Number).filter((_, i) => i % 2 !== 0);
+const files = [...fileInput].map(Number).filter((_, i) => i % 2 === 0);
+console.log(files, "\n", freeSpace);
