880715f1e5496bfc14caabed7fc53dcf10f81f04
grizeus <zaytsewww@gmail.com>
2024-12-12 19:21:38 +0200

    .

--- 09/pt-1.js
+++ 09/pt-1.js
@@ -1,3 +1,4 @@
+import { buffer } from "node:stream/consumers";
 import { pathBuilder, parseInput } from "../utils/utils.js";
 
 const path = pathBuilder("test.txt");
@@ -9,4 +10,36 @@ try {
 }
 const freeSpace = [...fileInput].map(Number).filter((_, i) => i % 2 !== 0);
 const files = [...fileInput].map(Number).filter((_, i) => i % 2 === 0);
-console.log(files, "\n", freeSpace);
+const limit = Math.ceil(fileInput.length / 2);
+
+let disk = "";
+for (let i = 0; i < limit; i++) {
+  if (i < files.length && files[i]){
+    for (let j = 0; j < files[i]; j++){
+      disk += i;
+    }
+  }
+  if (i < freeSpace.length && freeSpace[i]){
+    for (let j = 0; j < freeSpace[i]; j++){
+      disk += ".";
+    }
+  }
+}
+
+const diskArr = [...disk];
+
+for (let i = diskArr.length - 1; i > -1; i--) {
+  if (diskArr[i] !== ".") {
+    let buffer = diskArr[i];
+    diskArr[i] = ".";
+    for (let j = 0; j < disk.length; j++) {
+      if (diskArr[j] === ".") {
+        diskArr[j] = buffer;
+        break;
+      }
+    }
+  }
+}
+
+let res = diskArr.filter(a => a !== ".").reduce((acc, curr, i) => acc + (Number(curr) * i), 0);
+console.log(disk, diskArr, res);
