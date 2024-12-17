046bdc9b97861927625b9060145d29544dc6d706
grizeus <zaytsewww@gmail.com>
2024-12-17 16:50:33 +0200

    .

--- 14/pt-1.js
+++ 14/pt-1.js
@@ -1,6 +1,6 @@
 import { parseInput, pathBuilder, mod, print } from "../utils/utils.js";
 
-const rawData = await parseInput(pathBuilder("test.txt"));
+const rawData = await parseInput(pathBuilder("input.txt"));
 const robots = rawData
   .split("\r\n")
   .filter(Boolean)
@@ -8,25 +8,46 @@ const robots = rawData
     line.split(" ").map((subline) => {
       const l = subline.substring(subline.indexOf("=") + 1);
       return l.split(",").map(Number);
-    }),
+    })
   );
-const ROWS = 7;
-const COLS = 11;
+const ROWS = 103;
+const COLS = 101;
 const TICKS = 100;
-const grid = new Map();
-const incrementKey = (map, key) => {
-  if (map.has(key)) {
-    map.set(key, map.get(key) + 1);
-  } else {
-    map.set(key, 1);
-  }
-};
+const grid = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+
 for (const robot of robots) {
   const [coord, velocity] = robot;
   const [x, y] = coord;
   const [dx, dy] = velocity;
   const nx = mod(x + dx * TICKS, COLS);
   const ny = mod(y + dy * TICKS, ROWS);
-  incrementKey(grid, `${nx},${ny}`);
+  grid[ny][nx]++;
 }
-print(grid);
+const divideMatrix = (matrix, rowSplit, colSplit) => {
+  const quadrant1 = matrix
+    .slice(0, rowSplit)
+    .map((row) => row.slice(0, colSplit));
+  const quadrant2 = matrix
+    .slice(0, rowSplit)
+    .map((row) => row.slice(colSplit + 1));
+  const quadrant3 = matrix
+    .slice(rowSplit + 1)
+    .map((row) => row.slice(0, colSplit));
+  const quadrant4 = matrix
+    .slice(rowSplit + 1)
+    .map((row) => row.slice(colSplit + 1));
+
+  return { quadrant1, quadrant2, quadrant3, quadrant4 };
+};
+
+const rowSplit = Math.trunc(ROWS / 2);
+const colSplit = Math.trunc(COLS / 2);
+
+const dividedGrid = divideMatrix(grid, rowSplit, colSplit);
+
+let safetyFactor = 1;
+for (const key in dividedGrid) {
+  const buf = dividedGrid[key].flat(2).reduce((acc, a) => acc + a, 0);
+  safetyFactor *= buf;
+}
+print(safetyFactor);
